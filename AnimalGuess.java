7bf2072f6fc9553c41f8fea6156ab552fa4a4dcd
Isabelle Wang <ilwang@smith.edu>
2024-04-12 11:09:38 -0400

    Working

--- AnimalGuess.java
+++ AnimalGuess.java
@@ -1,35 +1,83 @@
 import java.util.Scanner;
 
 public class AnimalGuess {
+    DecisionTree<String> t;
 
-    public void checkInput(Scanner s) {
-        boolean valIn = false;
-        try {
-            if (input.toLowerCase().equals("yes") || input.toLowerCase().equals("y")) {
-                System.out.println();
+    public AnimalGuess() {
+        t = new DecisionTree<String>(null, null, null);
+    }
 
-            }
+    public static boolean checkInput(String input) {
+        if (input.toLowerCase().equals("yes") || input.toLowerCase().equals("y")) {
+            return true;
+        } else if (input.toLowerCase().equals("no") || input.toLowerCase().equals("n")) {
+            return false;
+        }
+    }
 
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Invalid input");
+    public static void addNode(String leaf, DecisionTree<String> t) {
+        Scanner input = new Scanner(System.in);
+        String animal;
+        System.out.println("I got it wrong. \nPlease help me learn. ");
+        System.out.println("What was your animal?");
+        animal = input.nextLine();
+
+        input.nextLine();
+        System.out.println("Type a question that would distinguish between a " + animal + " to a " + leaf);
+        String question = input.nextLine();
+
+        input.nextLine();
+        System.out.println("Would you answer yes to this question for the  " + animal + "?");
+        String response = input.nextLine();
+
+        if (checkInput(response)) {
+            
+
+
         }
 
     }
 
     public static void main(String[] args) {
-        DecisionTree left = new DecisionTree("Cat", new DecisionTree("Whale"), new DecisionTree("Rabbit"));
-        DecisionTree right = new DecisionTree("Dog", new DecisionTree("Chicken"), new DecisionTree("Pig"));
-        DecisionTree t = new DecisionTree("Rat", left, right);
+        AnimalGuess ag = new AnimalGuess();
+        DecisionTree<String> left = new DecisionTree<>("Bear");
+        DecisionTree<String> right = new DecisionTree<>("Moose");
+        DecisionTree<String> t = new DecisionTree<>("Is it big?", left, right);
 
-        DecisionTree node = t;
-        for (int i = 0; i < 1; i++) {
-            // ask questions
-            // user input
+        DecisionTree<String> currentNode = t;
+        System.out.println("Think of an animal. \nI'll try to guess it.");
+        Scanner input = new Scanner(System.in);
+        String in;
 
-            // check input to see if valid
+        while (!currentNode.isLeaf()) {
+            System.out.println(currentNode.getData());
+            in = input.nextLine();
 
-            // use input to move node
+            if (in.toLowerCase().equals("y") || in.toLowerCase().equals("yes")) {
+                currentNode = (DecisionTree<String>) currentNode.getRight();
+            } else if (in.toLowerCase().equals("n") || in.toLowerCase().equals("no")) {
+                currentNode = (DecisionTree<String>) currentNode.getLeft();
+            }
+        }
+
+        System.out.println("Is your animal a " + currentNode.getData() + "?");
+        input.nextLine();
+        in = input.nextLine();
+
+        if (in.toLowerCase().equals("n") || in.toLowerCase().equals("no")) {
+            addNode(currentNode.getData(), t);
+        } else {
+            System.out.println("We guessed it!");
         }
 
     }
 }
+
+// check input
+
+// ask questions
+// user input
+
+// check input to see if valid
+
+// use input to move node
--- DecisionTree.java
+++ DecisionTree.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
-public class DecisionTree extends BinaryTree<String> {
+public class DecisionTree<E> extends BinaryTree<E> {
 
     private String data;
 
     /** Left child of this node */
-    private DecisionTree left;
+    private DecisionTree<E> left;
 
     /** Right child of this node */
-    private DecisionTree right;
+    private DecisionTree<E> right;
 
-    public DecisionTree(String data) {
+    public DecisionTree(E data) {
         super(data);
     }
 
-    public DecisionTree(String data, DecisionTree left, DecisionTree right) {
+    public DecisionTree(E data, DecisionTree<E> left, DecisionTree<E> right) {
         super(data, left, right);
     }
 
-    public static <T> String preorderString(DecisionTree t) {
+    public static <T> String preorderString(DecisionTree<T> t) {
         if (t == null) {
             return "";
         } else {
@@ -26,7 +26,7 @@ public class DecisionTree extends BinaryTree<String> {
         }
     }
 
-    public static <T> String inorderString(DecisionTree t) {
+    public static <T> String inorderString(DecisionTree<T> t) {
         if (t == null) {
             return "";
         } else {
@@ -34,7 +34,7 @@ public class DecisionTree extends BinaryTree<String> {
         }
     }
 
-    public static <T> String postorderString(DecisionTree t) {
+    public static <T> String postorderString(DecisionTree<T> t) {
         if (t == null) {
             return "";
         } else {
@@ -42,23 +42,25 @@ public class DecisionTree extends BinaryTree<String> {
         }
     }
 
-    public String followPath(String path) {
-        DecisionTree node = this;
+    public E followPath(String path) {
+        DecisionTree<E> node = this;
 
         for (char letter : path.toCharArray()) {
             if (letter == 'Y') {
-                node = (DecisionTree) node.getLeft();
+                node = (DecisionTree<E>) node.getLeft();
             } else if (letter == 'N') {
-                node = (DecisionTree) node.getRight();
+                node = (DecisionTree<E>) node.getRight();
             }
         }
         return node.getData();
     }
 
     public static void main(String[] args) {
-        DecisionTree left = new DecisionTree("Cat", new DecisionTree("Whale"), new DecisionTree("Rabbit"));
-        DecisionTree right = new DecisionTree("Dog", new DecisionTree("Chicken"), new DecisionTree("Pig"));
-        DecisionTree t = new DecisionTree("Rat", left, right);
+        DecisionTree<String> left = new DecisionTree<String>("Cat", new DecisionTree<String>("Whale"),
+                new DecisionTree<String>("Rabbit"));
+        DecisionTree<String> right = new DecisionTree<String>("Dog", new DecisionTree<String>("Chicken"),
+                new DecisionTree<String>("Pig"));
+        DecisionTree<String> t = new DecisionTree<>("Rat", left, right);
         System.out.println(t);
         System.out.println(t.getData()); // return node
 
